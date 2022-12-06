167a23c8a91e27de02a170128063fe0c50417180
Tecnico <Tecnico@NB-INFOR-001006.FUTURO.LOCAL>
2022-12-06 14:42:34 -0300

    cambio de ruta de img QR

--- src/infrastructure/repositories/ws.external.ts
+++ src/infrastructure/repositories/ws.external.ts
@@ -71,7 +71,7 @@ class WsTransporter extends Client implements LeadExternal {
     const path = `${process.cwd()}/tmp`;
     let qr_svg = imageQr(base64, { type: "svg", margin: 4 });
     qr_svg.pipe(require("fs").createWriteStream(`${path}/qr.svg`));
-    qr_svg.pipe(require("fs").createWriteStream(`C:/Enviador/EnviadorWebAPP/src/assets/img/qr.svg`));
+    qr_svg.pipe(require("fs").createWriteStream(`C:/Enviador/EnviadorWebAPP/dist/enviador/assets/img/qr.svg`));
     console.log(`⚡ Recuerda que el QR se actualiza cada minuto ⚡'`);
     console.log(`⚡ Actualiza F5 el navegador para mantener el mejor QR⚡`);
   };
